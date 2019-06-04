5c8ce983775ce7f0da61fa72358973031f15ee1e
Anthony Trinh <trinhan@oregonstate.edu>
2019-06-04 00:53:12 -0700

    Added some more CSS

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-var getStartedButton = document.getElementById("create-resume-button");
-var modal = document.getElementById("modal");
-var closeButton = document.getElementById("close-modal");
+var getStartedButton = document.getElementById('create-resume-button');
+var modals = document.getElementsByClassName('modal');
+var closeButton = document.getElementsByClassName('close-button');
+var nextButton = document.getElementsByClassName('next-button');
+
+
 
     getStartedButton.addEventListener('click', function() {
         //Open modal when 'Get Started' is clicked
-        modal.style.display = 'block';
-    });
-    
-    closeButton.addEventListener('click', function() {
-        //Close modal when 'X' is clicked
-        modal.style.display = 'none';
+        modals[0].classList.remove('hidden');
     });
 
+    /*
     window.addEventListener('click', function(event) {
         //Close modal if user clicks dark area outside form outline
        if (event.target === modal) {
-           modal.style.display = 'none';
-       } 
+           modal[0].classList.toggle('hidden');
+       }
     });
+    */
 
 
+
+    for (let a = 0; a < nextButton.length; a++) {
+      console.log(a);
+      nextButton[a].addEventListener('click', function () {
+        console.log("Called next Button");
+        modals[a].classList.add('hidden');
+        modals[a + 1].classList.toggle('hidden');
+      });
+    };
